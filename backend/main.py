745cf9e1982fc8e4be23d3b5787026c538dc74da
Nathan Prisbrey <ntpbanana@gmail.com>
2022-08-10 01:42:04 -0700

    Created Basic WebSocket Tests

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,13 @@
-import socket
+import asyncio
+import websockets
+
+async def echo(websocket):
+	async for message in websocket:
+		print("Echoing message '",message,"' now...")
+		await websocket.send(message)
+
+async def main():
+	async with websockets.serve(echo, "localhost", 8080):
+		await asyncio.Future() #run forever
+
+asyncio.run(main())
